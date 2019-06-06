e872d53b42c2786e16c5f5aadf8501b487f3c62b
Manjari Senthilkumar <manjari.infinite@gmail.com>
2019-06-06 05:56:43 -0700

    Charge class completed

--- finalproject-team-2/Charge.java
+++ finalproject-team-2/Charge.java
@@ -1,11 +1,30 @@
-
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.*;
+import javafx.scene.image.*;
 /**
  * Write a description of class Charge here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Manjari Senthilkumar
+ * @version 6/6/2019
  */
 public class Charge
 {
+    private Node platform;
+    private int width, height;
+    /**
+     * Constructor for objects of class Charge
+     */
+    public Charge(Pane root, int j, int i)
+    {
+        width = 100;//(int)pic.getFitWidth();
+        height = 200;//(int)pic.getFitHeight();
+        platform = Game.createEntity(j*60, i*60, width, height, Color.DARKSEAGREEN, root);
+    }
     
+    public Node getNode(){return platform;}
+    
+    public int getWidth(){return width;}
+    
+    public int getHeight(){return height;}
 }
