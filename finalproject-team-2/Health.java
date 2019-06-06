9aaca013ee1965e0de62b2746fe39680313c8a0b
Manjari Senthilkumar <manjari.infinite@gmail.com>
2019-06-06 05:48:29 -0700

    attempted several resize things - didn't work

--- finalproject-team-2/Health.java
+++ finalproject-team-2/Health.java
@@ -12,15 +12,24 @@ public class Health
     ImageView barimgV;
     public Health(Pane root) throws Exception
     {
-        barimgV = Game.convertImageView("healthbar.png");
+        barimgV = Game.convertImageView("C:\\Users\\Manjari\\Desktop\\platform game\\graphics\\healthbar.png");
         barimgV.setX(160);
         barimgV.setY(100);
-        barimgV.setPreserveRatio(true);
-        barimgV.setFitWidth(200);
-        barimgV.setFitHeight(20);
-        
+        //barimgV.setPreserveRatio(true);
+        //barimgV.setFitWidth(200);
+        //barimgV.setFitHeight(20);
+        barimgV.setLayoutX(200);
         root.getChildren().add(barimgV);
+        
+        barimgV.translateYProperty().addListener((obs, old, newValue) -> {
+                int offset = newValue.intValue();
+                if(offset > 60 && offset < 3000 - 120){
+                    root.setLayoutY(-(offset - 800));
+                }
+            });
     }
     public void update()
-    {}
+    {
+        //change imageViewport with the change Listener
+    }
 }
