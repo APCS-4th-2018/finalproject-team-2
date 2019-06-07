130de1bc835dbc420b442b822054a6b8181ca81f
meghak13 <42729620+meghak13@users.noreply.github.com>
2019-06-07 01:48:50 -0700

    add comments and javadocs to LevelData

--- finalproject-team-2/LevelData.java
+++ finalproject-team-2/LevelData.java
@@ -1,6 +1,8 @@
  
 /**
- * Write a description of class LevelData here.
+ * Creates and returns and array of Strings where each String represents a row in the game and has different values ranging from
+ *0 to 5. Each value, except 0, signifies an action: 1 means build a block, 2 means build a lift, 3 means build a ladder, 4 
+ *means build a portal, and 5 means build a charge station. 
  *
  * @author Manjari Senthilkumar
  * @version 05/31/2019
@@ -59,5 +61,10 @@ public class LevelData
         "000000000000",
         "111111111111"
     };
+    /**
+     * returns array of Strings containing information on what to build in each row of the game
+     *
+     * @return            array of Strings containing information on what to build in each row of the game
+     */
     public static String[] getLevel1(){return LEVEL1;}
 }
